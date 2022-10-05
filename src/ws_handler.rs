1e2d4c3ad28f161fc53353a0f684bc6802044d8c
Raphael Darley <raphael@raphaeldarley.com>
2022-10-05 22:49:51 +0000

    add mpsc channel to forward to websocket

--- src/ws_handler.rs
+++ src/ws_handler.rs
@@ -1,8 +1,11 @@
 use std::collections::BTreeMap;
 
-use futures_util::{SinkExt, StreamExt};
+use futures_util::stream::SplitSink;
+use futures_util::{SinkExt, StreamExt, TryFutureExt};
 use serde::Deserialize;
 use surrealdb::sql::{Strand, Value};
+use tokio::sync::mpsc::{self, UnboundedSender};
+use tokio_stream::wrappers::UnboundedReceiverStream;
 use warp::ws::WebSocket;
 use warp::ws::{self, Message};
 
@@ -12,26 +15,29 @@ use crate::State;
 pub async fn ws_connection(ws: WebSocket, state: State) {
     let (mut ws_tx, mut ws_rx) = ws.split();
 
+    let (tx, rx) = mpsc::unbounded_channel();
+    let mut rx = UnboundedReceiverStream::new(rx);
+
+    tokio::spawn(async move {
+        while let Some(message) = rx.next().await {
+            ws_tx
+                .send(message)
+                .unwrap_or_else(|e| println!("Websocket error: {}", e))
+                .await;
+        }
+    });
+
     while let Some(msg_result) = ws_rx.next().await {
         match msg_result {
             Ok(msg) => {
-                // println!("{:?}", msg);
-                // add_visit_record(&state.dsconn).await.unwrap();
-                // print_db_visits(&state.dsconn).await.unwrap();
-                // let msg = db_visits_as_json(&state.dsconn).await.unwrap();
-
-                // match ws_tx.send(ws::Message::text(msg)).await {
-                //     Ok(_) => {}
-                //     Err(e) => println!("Websocket ERROR: {:?}", e),
-                // }
-
                 if msg.is_text() {
-                    if let Some(reply) = handle_text_msg(msg, state.clone()).await {
-                        match ws_tx.send(reply).await {
-                            Ok(_) => {}
-                            Err(e) => println!("Websocket ERROR: {:?}", e),
-                        }
-                    };
+                    // if let Some(reply) = handle_text_msg(msg, state.clone()).await {
+                    //     match ws_tx.send(reply).await {
+                    //         Ok(_) => {}
+                    //         Err(e) => println!("Websocket ERROR: {:?}", e),
+                    //     }
+                    // };
+                    tokio::spawn(handle_text_msg(msg, state.clone(), tx.clone()));
                 } else if msg.is_binary() {
                     println!("Websocket ERROR: Binary message not supported");
                     println!("{:?}", msg);
@@ -47,7 +53,11 @@ pub async fn ws_connection(ws: WebSocket, state: State) {
     }
 }
 
-async fn handle_text_msg(message: Message, state: State) -> Option<Message> {
+async fn handle_text_msg(
+    message: Message,
+    state: State,
+    tx: UnboundedSender<Message>,
+) -> Option<Message> {
     let msg = message.to_str().unwrap();
 
     let req: Req = match serde_json::from_str(msg) {
@@ -61,13 +71,17 @@ async fn handle_text_msg(message: Message, state: State) -> Option<Message> {
     println!("{:?}", req);
 
     match req {
-        Req::Create(c) => handle_create(c, state).await,
-        Req::Read(r) => handle_read(r, state).await,
+        Req::Create(c) => handle_create(c, state, tx).await,
+        Req::Read(r) => handle_read(r, state, tx).await,
         _ => None,
     }
 }
 
-async fn handle_create(req: CreateReq, state: State) -> Option<Message> {
+async fn handle_create(
+    req: CreateReq,
+    state: State,
+    tx: UnboundedSender<Message>,
+) -> Option<Message> {
     let mut vars = BTreeMap::new();
     vars.insert(
         "text".to_string(),
@@ -83,10 +97,26 @@ async fn handle_create(req: CreateReq, state: State) -> Option<Message> {
         .await
         .unwrap();
 
-    None
+    //from read handler sends back rows with changes
+    let select_response = state
+        .dsconn
+        .execute("SELECT * FROM item", None, false)
+        .await
+        .unwrap();
+
+    let select_result = select_response[0].output().unwrap();
+
+    if let Value::Array(rows) = select_result {
+        let reply = Message::text(serde_json::to_string(rows).unwrap());
+        println!("{:?}", reply);
+        tx.send(reply).unwrap();
+        None
+    } else {
+        panic!("DB vists hasn't returned array of rows")
+    }
 }
 
-async fn handle_read(req: ReadReq, state: State) -> Option<Message> {
+async fn handle_read(req: ReadReq, state: State, tx: UnboundedSender<Message>) -> Option<Message> {
     // let mut vars = BTreeMap::new();
     // vars.insert(
     //     "text".to_string(),
@@ -101,7 +131,10 @@ async fn handle_read(req: ReadReq, state: State) -> Option<Message> {
     let select_result = select_response[0].output().unwrap();
 
     if let Value::Array(rows) = select_result {
-        Some(Message::text(serde_json::to_string(rows).unwrap()))
+        let reply = Message::text(serde_json::to_string(rows).unwrap());
+        println!("{:?}", reply);
+        tx.send(reply).unwrap();
+        None
     } else {
         panic!("DB vists hasn't returned array of rows")
     }
